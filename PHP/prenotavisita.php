daa8067e91e10e6aa2a220fe157a8f9972284381
Ivan Furlan <ivanfurlan98@gmail.com>
2019-12-19 17:12:11 +0100

    Prenotavisita aggiunto controllo orario

--- PHP/prenotavisita.php
+++ PHP/prenotavisita.php
@@ -19,7 +19,7 @@ $paginaHTML = getPaginaHTML($_SERVER["PHP_SELF"]);
 $erroriDaMostrare = '';
 
 //se si sono ricevuti i parametri per inserire una nuova visita
-if (isset($_SESSION['emailUtente'], $_POST['giorno'], $_POST['mese'], $_POST['anno'], $_POST['tipovisita'], $_POST['orario']) && $_POST['orario'] != '') {
+if (isset($_SESSION['emailUtente'], $_POST['giorno'], $_POST['mese'], $_POST['anno'], $_POST['tipovisita'])) {
     //devo inserire una nuova visita come prenotata
     $giorno = $_POST['giorno'];
     $mese = $_POST['mese'];
@@ -28,10 +28,13 @@ if (isset($_SESSION['emailUtente'], $_POST['giorno'], $_POST['mese'], $_POST['an
     //faccio dei controlli sulla data inserita
     if (!checkdate($mese, $giorno, $anno)) {
         //la data non è valida
-        $erroriDaMostrare .= "<li>La data scelta non può essere già passata. Selezionare una data futura.</li>";
+        $erroriDaMostrare .= "<li>La data scelta non esiste. Selezionare una data esistente.</li>";
     } elseif (dataGiaPassata($giorno, $mese, $anno)) {
         //la data è antecedente alla data corrente
         $erroriDaMostrare .= "<li>La data scelta non può essere già passata. Selezionare una data futura.</li>";
+    } elseif (!(isset($_POST['orario']) && !$_POST['orario'])) {
+        //non è stato impostato un orario
+        $erroriDaMostrare .= "<li>Nessun orario selezinoato. Selezionato un orario</li>";
     } elseif (!$oggettoConnessione->dataOraVisitaDisponibile($giorno, $mese, $anno, $_POST['orario'])) {
         ///data e ora selezionati non sono disponibili
         $erroriDaMostrare .= "<li>La data e l'orario scelti non sono disponibili.</li>";
@@ -59,13 +62,13 @@ if (isset($_SESSION['emailUtente'])) {
     $pageContent .= file_get_contents('HTML/prenotavisita_form.html');
 
     $visitePrenotate .= '<h2>Le mie visite future</h2>';
-    $temp = preparaHTMLListaVisite($oggettoConnessione->getListaVisitePrenotatePeriodo("of",$_SESSION['emailUtente']));
-    $visitePrenotate.= ($temp)?$temp:"<li>Non sono presenti visite prenotate per oggi o nei giorni futuri</li>";
+    $temp = preparaHTMLListaVisite($oggettoConnessione->getListaVisitePrenotatePeriodo("of", $_SESSION['emailUtente']));
+    $visitePrenotate .= ($temp) ? $temp : "<li>Non sono presenti visite prenotate per oggi o nei giorni futuri</li>";
     $visitePrenotate .= '</ul>';
 
     $visitePrenotate .= '<h2>Le mie ultiime 20 visite passate</h2>';
-    $temp = preparaHTMLListaVisite($oggettoConnessione->getListaVisitePrenotatePeriodo("p",$_SESSION['emailUtente']));
-    $visitePrenotate.= ($temp)?$temp:"<li>Non sono presenti visite passate</li>";
+    $temp = preparaHTMLListaVisite($oggettoConnessione->getListaVisitePrenotatePeriodo("p", $_SESSION['emailUtente']));
+    $visitePrenotate .= ($temp) ? $temp : "<li>Non sono presenti visite passate</li>";
     $visitePrenotate .= '</ul>';
 } else {
     //Non è stato effettuato l'acecsso
@@ -78,6 +81,15 @@ if (isset($_SESSION['emailUtente'])) {
 if ($erroriDaMostrare !== '') {
     //se ci sono degli errori da stampare, preparo la stringa dentro ad un div
     $erroriDaMostrare = '<div class="erroreCampiForm"><ul>' . $erroriDaMostrare . '</ul></div>';
+
+    //torno impostare i campi del form con ciò che aveva inserito l'utente
+    $pageContent = str_replace('value="' . $_POST['tipovisita'] . '"', 'value="' . $_POST['tipovisita'] . '" selected="selected"', $pageContent);
+    $pageContent = str_replace('value="' . $_POST['anno'] . '"', 'value="' . $_POST['anno'] . '" selected="selected"', $pageContent);
+
+    //l'orario torna utile solo per chi non ha javascript, per gli altri viene comunque resettato dalla funzione js
+    if (isset($_POST['orario'])) {
+        $pageContent = str_replace('value="' . $_POST['orario'] . '"', 'value="' . $_POST['orario'] . '" checked="checked"', $pageContent);
+    }
 }
 
 $paginaHTML = str_replace("<erroriDaMostrare />", $erroriDaMostrare, $paginaHTML);
